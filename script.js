47af7f795c6b309d470f7d77e6e63600238d9420
gtsoul-tech <56584633+gtsoul-tech@users.noreply.github.com>
2023-05-07 19:56:56 +0300

    Add rgb and shading

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 let dimension= 16;
+function drawRgb(){
+    const container = document.querySelector("#container");
+    container.replaceChildren();
+    for(let j=0;j<dimension;j++){
+        const row = document.createElement('div');
+        row.classList.add("row");
+        for(let i=0;i<dimension;i++){
+            const col = document.createElement('div');
+            col.classList.add("col");
+            col.style.backgroundColor= "rgb(255, 255, 255)";
+            row.appendChild(col);
+            col.addEventListener('mouseover', () =>{ addRgbColor(col);});
+        }
+        container.appendChild(row);
+    }
+}
 function draw(){
     const container = document.querySelector("#container");
     container.replaceChildren();
@@ -8,13 +24,40 @@ function draw(){
         for(let i=0;i<dimension;i++){
             const col = document.createElement('div');
             col.classList.add("col");
+            col.style.backgroundColor= "rgb(255, 255, 255)";
             row.appendChild(col);
-            col.addEventListener('mouseover', () => {col.classList.add("black");});
+            col.addEventListener('mouseover', () =>{ blackToWhite(col);});
         }
         container.appendChild(row);
     }
 }
-function getDimension(){
+function addRgbColor(column){
+    if(column.style.backgroundColor === "rgb(255, 255, 255)"){
+        column.style.backgroundColor= getRandomRgb();
+    }else{
+        let r = /\d+/;
+        if(column.style.filter === ""){
+            column.style.filter = "brightness(100%)";
+        }else{
+            let previousBrightness = parseInt(column.style.filter.match(r).join(),10);
+            column.style.filter = `brightness( ${previousBrightness-10}%)`;
+        }
+    }
+}
+function blackToWhite(column){
+    column.style.backgroundColor = "rgb(0,0,0)";
+}
+function getDimensionRgb(){
+    dimension = parseInt(prompt("Give me the dimensions:"),10);
+    if(isNaN(dimension) || dimension > 100){
+        alert("You didnt give a number or it was greater than 100(default 16)");
+        dimension=16;
+        drawRgb();
+    }else{
+        drawRgb();
+    }
+}
+function getDimensionBlack(){
     dimension = parseInt(prompt("Give me the dimensions:"),10);
     if(isNaN(dimension) || dimension > 100){
         alert("You didnt give a number or it was greater than 100(default 16)");
@@ -24,8 +67,11 @@ function getDimension(){
         draw();
     }
 }
-draw();
+drawRgb();
 
 function getRandomRgb(){
-    return Math.floor(Math.random() * 256);
+    let red= Math.floor(Math.random() * 256);
+    let green= Math.floor(Math.random() * 256);
+    let blue= Math.floor(Math.random() * 256);
+    return  "rgb(" + red + "," + green + "," + blue + ")";
 }
